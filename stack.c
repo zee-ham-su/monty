6ed006e6180f67148e7fd08b2ae74c00f55a29c4
zee-ham-su <hsufiian@yahoo.com>
2023-06-21 14:58:56 -0700

    monty update

---
+++ stack.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+/**
+ * f_stack - Sets the lifi variable to 0.
+ * @head: Pointer to the stack head
+ * @counter: Line number
+ * Return: No return value
+ */
+void f_stack(stack_t **head, unsigned int counter)
+{
+(void)head;
+(void)counter;
+bus.lifi = 0;
+}
